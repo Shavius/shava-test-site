8a528eda77dbe9623692208fe6e84da157c5aa59
Shavius <glamurparty@ukr.net>
2024-07-05 18:50:06 +0300

    Git Init

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: [
+		"eslint:recommended",
+		"plugin:@typescript-eslint/recommended",
+		"plugin:@typescript-eslint/stylistic",
+		"airbnb-base",
+		"plugin:import/typescript",
+		"plugin:prettier/recommended",
+	],
+	overrides: [],
+	parserOptions: {
+		ecmaVersion: "latest",
+		sourceType: "module",
+		project: "./tsconfig.json",
+	},
+	plugins: ["prettier"],
+	rules: {
+		"import/extensions": [
+			"error",
+			"ignorePackages",
+			{
+				ts: "never",
+			},
+		],
+		"@typescript-eslint/explicit-function-return-type": [
+			"error",
+			{
+				allowExpressions: false,
+				allowTypedFunctionExpressions: true,
+				allowHigherOrderFunctions: false,
+				allowDirectConstAssertionInArrowFunctions: false,
+				allowConciseArrowFunctionExpressionsStartingWithVoid: false,
+			},
+		],
+		"no-console": "off",
+		"func-names": "off",
+		"lines-between-class-members": "off",
+	},
+};
---
+++ src/ts/script.ts
@@ -0,0 +1,5 @@
+import "../index.html";
+import "../404.html";
+import "../scss/style.scss";
+
+console.log("Hello");
